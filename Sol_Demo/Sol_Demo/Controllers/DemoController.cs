44a9c642990390398b0da489b7600c3567f176eb
Kishor Naik <kishor.naik011.net@gmail.com>
2021-05-16 09:20:12 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Controllers/DemoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sol_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/demo")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        private readonly MySettingModel mySettingModel = null;
+
+        public DemoController(IOptions<MySettingModel> options)
+        {
+            mySettingModel = options.Value;
+        }
+
+        [HttpGet("get-mysetting-values")]
+        public IActionResult GetMySettingValue()
+        {
+            return base.Ok(mySettingModel);
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Models/MySettingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Models
+{
+    public class MySettingModel
+    {
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+    }
+}
